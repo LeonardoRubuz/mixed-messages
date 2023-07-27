e20efbaea3776643032213a9549a25a9d75a10e2
LeonardoRubuz <rubuzleonardo@gmail.com>
2023-07-27 10:56:31 +0100

    array of months objects created

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
 /* Little commentary to see if my commits are
 directly push to my GitHub account
 */
+
+// first step : create an array that contains months objects
+let months = [
+    {
+        "Janvier" : []
+    },
+    {
+        "Février" : []
+    },
+    {
+        "Mars" : []
+    },
+    {
+        "Avril" : []
+    },
+    {
+        "Mai" : []
+    },
+    {
+        "Juin" : []
+    },
+    {
+        "Juillet" : []
+    },
+    {
+        "Août" : []
+    },
+    {
+        "Septembre" : []
+    },
+    {
+        "Octobre" : []
+    },
+    {
+        "Novembre" : []
+    },
+    {
+        "Décembre" : []
+    },
+]
